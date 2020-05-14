847390a52b2369e7bc7e597683d07d9a6053ecb4
sergiyii <sergeyfil83@mail.ru>
2020-05-14 17:00:28 +0500

    ItogTask 1

--- src/ItogTask1.java
+++ src/ItogTask1.java
@@ -3,17 +3,20 @@ import java.util.Scanner;
 public class ItogTask1 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Введите двоичное число: ");
+        System.out.print("Введите двоичное число: ");
         String str = new String(sc.nextLine()).trim();
         int znDec = 0;
         int mn = 1;
-
-        for (int i = str.length() - 1; i >= 0; i--){
-            if(str.charAt(i) == '1'){
-                znDec = znDec + mn;
+        if(str.matches("[01]+")) {
+            for (int i = str.length() - 1; i >= 0; i--) {
+                if (str.charAt(i) == '1') {
+                    znDec = znDec + mn;
+                }
+                mn = mn * 2;
             }
-            mn = mn * 2;
+            System.out.println(str + " = " + znDec);
+        } else {
+            System.out.println("Введенное значение не является двоичным числом!");
         }
-        System.out.println(str + " = " + znDec);
     }
 }
